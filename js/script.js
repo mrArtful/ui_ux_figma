574aada4e5540c69bced9d32d6c3d1a8635cff07
Roman Kutselepa <roman.kutselepa@gmail.com>
2026-01-17 17:14:11 +0200

    latest

--- js/script.js
+++ js/script.js
@@ -1,11 +1,44 @@
+const wrapper = document.querySelector(".form-wrapper");
 const getStartedBtn = document.querySelector('#get-started');
-const form = document.querySelector(".get-started-form");
 const formCloseBtn = document.querySelector(".form-close-btn");
+const menu = document.querySelector('.navigation');
+const menuBtn = document.querySelector('#burger-menu');
+const menuCloseBtn = document.querySelector('#menu-close');
 
-getStartedBtn.addEventListener('click', () => {
-    form.classList.toggle('hidden');
+function toggleMenu() {
+    menu.classList.toggle("visible");
+}
+
+function closeMenu() {
+    menu.classList.remove("visible");
+}
+
+function openForm() {
+    wrapper.classList.add("is-open");
+    wrapper.classList.remove("is-closed");
+}
+
+function closeForm() {
+    wrapper.classList.remove("is-open");
+    wrapper.classList.add("is-closed");
+    
+}
+
+window.addEventListener('load', () => {
+    if (window.innerWidth < 1400) {
+        closeForm();
+    }
 })
 
-formCloseBtn.addEventListener('click', () => {
-    form.classList.add("hidden");
+window.addEventListener("resize", () => {
+    if (window.innerWidth < 1400) {
+        closeForm();
+    } else {
+        openForm();
+    }
 })
+
+getStartedBtn.addEventListener("click", openForm);
+formCloseBtn.addEventListener("click", closeForm);
+menuBtn.addEventListener('click', toggleMenu);
+menuCloseBtn.addEventListener('click', closeMenu);
